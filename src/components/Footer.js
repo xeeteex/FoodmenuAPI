c725783be5c41fcfc5a45670cf2422b7590579d4
Kshitij <xeeteexstha@gmail.com>
2024-07-03 21:40:02 +0545

    changes1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='relative'>
-     <svg className='sm:hidden' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e8c34a" fillOpacity="1" d="M0,256L1440,0L1440,320L0,320Z"></path></svg>
+     <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e8c34a" fillOpacity="1" d="M0,256L1440,0L1440,320L0,320Z"></path></svg>
 
       
 
